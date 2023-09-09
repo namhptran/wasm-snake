51f6f358c888736aadf048a7e865104d316d680c
namhptran <nam.hp.tran@gmail.com>
2023-09-09 16:17:46 +1000

    Complete tutorial

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,24 @@
 #[cfg(feature = "buddy-alloc")]
 mod alloc;
+mod game;
+mod palette;
+mod snake;
 mod wasm4;
-use wasm4::*;
 
-#[rustfmt::skip]
-const SMILEY: [u8; 8] = [
-    0b11000011,
-    0b10000001,
-    0b00100100,
-    0b00100100,
-    0b00000000,
-    0b00100100,
-    0b10011001,
-    0b11000011,
-];
+use game::Game;
+use lazy_static::lazy_static;
+use std::sync::Mutex;
+
+lazy_static! {
+    static ref SNAKE_GAME: Mutex<Game> = Mutex::new(Game::new());
+}
+
+#[no_mangle]
+fn start() {
+    palette::set_palette([0xfbf7f3, 0xe5b083, 0x426e5d, 0x20283d])
+}
 
 #[no_mangle]
 fn update() {
-    unsafe { *DRAW_COLORS = 2 }
-    text("Hello from Rust!", 10, 10);
-
-    let gamepad = unsafe { *GAMEPAD1 };
-    if gamepad & BUTTON_1 != 0 {
-        unsafe { *DRAW_COLORS = 4 }
-    }
-
-    blit(&SMILEY, 76, 76, 8, 8, BLIT_1BPP);
-    text("Press X to blink", 16, 90);
+    SNAKE_GAME.lock().expect("game_state").update();
 }
