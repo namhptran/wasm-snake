9dc649d35a487f73d1c307882980b901e94b2c2c
Nam Tran <nam.hp.tran@gmail.com>
2023-09-10 15:44:45 +1000

    Mess with formatting to check rustfmt

--- src/game.rs
+++ src/game.rs
@@ -77,9 +77,7 @@ impl Game {
             self.snake.up();
         } else if just_pressed & wasm4::BUTTON_DOWN != 0 {
             self.snake.down();
-        } else if just_pressed & wasm4::BUTTON_2 != 0 {
-            self.reset();
-        }
+        } else if just_pressed & wasm4::BUTTON_2 != 0 {self.reset();}
 
         self.prev_gamepad = gamepad;
     }
